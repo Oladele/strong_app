940426ec585cd947151be09ab85c9b04c8e21616
Dele <oladele@gmail.com>
2013-01-05 16:51:57 -0500

    Tune css and rspec.Begin User Controler

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,53 +2,34 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-	let(:base_title) { "Strongla" }
-  
-	describe "Home page" do
+	#let(:base_title) { "Strongla" }
+  	subject { page }
 
-		it "should have the h1 'Strongla'" do
-		  visit '/static_pages/home'
-		  page.should have_selector('h1', text:'Strongla')
-		end
-		it "should have the title 'Strongla|Home'" do
-		  visit '/static_pages/home'
-		  page.should have_selector('title', text:"#{base_title}|Home")
-		end
+	describe "Home page" do
+		before { visit root_path }
+
+		it { should have_selector('h1', 	text:'Strongla') }
+		it { should have_selector('title', 	text: full_title('Home')) }
 	end
 
 	describe "Help page" do
+		before { visit help_path }
 
-		it "should have the h1 'Help'" do
-		  visit '/static_pages/help'
-		  page.should have_selector('h1', text:'Help')
-		end
-		it "should have the title 'Strongla|Help'" do
-		  visit '/static_pages/help'
-		  page.should have_selector('title', text:"#{base_title}|Help")
-		end
+		it { should have_selector('h1', text:'Help') }
+		it { should have_selector('title', text:full_title('Help')) }
 	end
 
 	describe "About page" do
+		before { visit about_path }
 
-		it "should have the h1 'About Strongla'" do
-		  visit '/static_pages/about'
-		  page.should have_selector('h1', text:'About Strongla')
-		end
-		it "should have the title 'Strongla|About'" do
-		  visit '/static_pages/about'
-		  page.should have_selector('title', text:"#{base_title}|About")
-		end
+		it { should have_selector('h1', text:'About Strongla') }
+		it { should have_selector('title', text:full_title('About')) }
 	end
 
 	describe "Contact page" do
+		before { visit contact_path }
 
-		it "should have the h1 'Contact Us'" do
-		  visit '/static_pages/contact'
-		  page.should have_selector('h1', text:'Contact Us')
-		end
-		it "should have the title 'Strongla|Contact'" do
-		  visit '/static_pages/contact'
-		  page.should have_selector('title', text:"#{base_title}|Contact")
-		end
+		it { should have_selector('h1', text:'Contact Us') }
+		it { should have_selector('title', text:full_title('Contact')) }
 	end
 end
